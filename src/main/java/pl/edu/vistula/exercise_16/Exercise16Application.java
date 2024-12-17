9dec87092ce075099c3c80fafc140423fec93605
Piotr Sieradzki <piotr.sieradzki03@gmail.com>
2024-12-17 17:07:26 +0100

    Home Assignment - Exercise 16
    Programowanie Java - Praca domowa - zadanie 1

---
+++ src/main/java/pl/edu/vistula/exercise_16/Exercise16Application.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.exercise_16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise16Application.class, args);
+	}
+
+}
